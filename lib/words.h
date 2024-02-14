82a2840ff49ef529b32296d37e6477132d0ee1ee
Ishan Pranav <ishan.pranav@outlook.com>
2024-02-14 13:33:55 -0500

    Document words library

--- lib/words.h
+++ lib/words.h
@@ -3,14 +3,20 @@
 #include "euler.h"
 
 /**
+ * Converts a non-negative integer into its English string representaiton.
  * 
- * @param value
- * @return 
+ * @param value the integral value.
+ * @return Fully expanded English string representation of the given value, or
+ *         `NULL` if there is not enough memory to complete the operation. The
+ *         caller is responsible.
 */
 String words_to_string(int value);
 
 /**
+ * Solves Project Euler Problem 17 (Number Letter Counts) by computing the total
+ * number of letters in the formal English string representation of the first
+ * one thousand positive integers.
  * 
- * @return 
+ * @return The solution to Project Euler Problem 17.
 */
 int words_number_letter_counts(void);
