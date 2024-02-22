60b2d5c15711a51bb795b542c4430efc688b055e
Ishan Pranav <ishan.pranav@outlook.com>
2024-02-21 20:31:41 -0500

    Document reverse Polish notation

--- lib/rpn.h
+++ lib/rpn.h
@@ -2,4 +2,11 @@
 
 #include "euler.h"
 
+/**
+ * Evaluates a sequence of tokens in reverse Polish notation.
+ * 
+ * @param tokens the sequence of tokens consisting of operators and operands.
+ * @param length the number of tokens.
+ * @return The evaluated expression.
+*/
 int rpn_evaluate(String tokens[], size_t length);
