0d7c9d8dacaa6c09ba3de13b42d940fa719f86ac
ishanpranav <ishan.pranav@outlook.com>
2024-05-16 17:47:50 -0700

    Update random.h

--- lib/random.h
+++ lib/random.h
@@ -9,11 +9,11 @@ typedef long long (*Random)(void* instance, long long max);
 /**
  * Generates a random integer in the interval [0, `max`) using Blackman and
  * Vigna's xoshiro256** (exclusive or-shift-rotate, 256-bit) algorithm.
- * 
+ *
  * @param instance the instance.
  * @param max      the exclusive upper bound of the generated integer.
  * @return A random integer in the interval [0, `max`). Only the least 64 bits
- *         of this value are necessarily uniformly distributed. 
+ *         of this value are necessarily uniformly distributed.
 */
 long long xoshiro256_star_star_random(Object instance, long long max);
 
@@ -21,8 +21,28 @@ long long xoshiro256_star_star_random(Object instance, long long max);
  * Generates a random 64-bit integer using Blackman and Vigna's xoshiro256**
  * (exclusive or-shift-rotate, 256-bit) algorithm, and returns a value truncated
  * to exactly 64 bits.
- * 
+ *
  * @param instance the instance.
  * @return A uniformly-distributed random 64-bit integer in the interval.
 */
 uint64_t xoshiro256_star_star_random64(Object instance);
+
+/**
+ * Generates a random integer in the interval [0, `max`] using Marsaglia's
+ * xorshift32 (exclusive or, 32-bit) algorithm.
+ *
+ * @param instance the instance.
+ * @param max      the exclusive upper bound of the generated integer.
+ * @return A random integer in the interval [0, `max`]. Only the least 32 bits
+ *         of this value are necessarily uniformly distributed.
+*/
+long long xorshift32_random(Object instance, long long max);
+
+/**
+ * Generates a random 32-bit integer in the interval [0, `max`] using
+ * Marsaglia's xorshift32 (exclusive or, 32-bit) algorithm.
+ *
+ * @param instance the instance.
+ * @return A uniformly distributed random 32-bit integer in the interval.
+*/
+uint32_t xorshift32_random32(Object instance);
---
+++ lib/randoms/xorshift32.c
@@ -0,0 +1,4 @@
+// Licensed under the MIT License.
+
+#include "../random.h"
+
