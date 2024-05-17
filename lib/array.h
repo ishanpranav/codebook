0d73343eb7fdd4ae962346d8b86317d68f1b49b1
ishanpranav <ishan.pranav@outlook.com>
2024-05-16 18:02:14 -0700

    Fix array header guard

--- lib/array.h
+++ lib/array.h
@@ -1,7 +1,7 @@
 // Licensed under the MIT License.
 
-#ifndef OBJECT_30074e429e344adaabeecbcb538bae15
-#define OBJECT_30074e429e344adaabeecbcb538bae15
+#ifndef ARRAY_30074e429e344adaabeecbcb538bae15
+#define ARRAY_30074e429e344adaabeecbcb538bae15
 
 /** Defines an array as a contiguous span of memory. */
 typedef void* Array;
