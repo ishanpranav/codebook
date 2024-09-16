e8b99fe2bf4caaa6da3c60079e89b9c0b624d1d8
Ishan Pranav <64936129+ishanpranav@users.noreply.github.com>
2024-09-16 10:47:26 -0400

    Update exception.h

--- lib/exception.h
+++ lib/exception.h
@@ -10,7 +10,10 @@ enum Exception
     EXCEPTION_OUT_OF_MEMORY = -1,
 
     /** A composite format string is not well formed. */
-    EXCEPTION_FORMAT = -2
+    EXCEPTION_FORMAT = -2,
+
+    /** The value of an argument is outside the allowable range. */
+    EXCEPTION_ARGUMENT_OUT_OF_RANGE = -3
 };
 
 /** Specifies an error that occurs at runtime. */
